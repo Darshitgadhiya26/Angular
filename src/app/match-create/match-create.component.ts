34d8b7b1b79be0b3ce7fde15cb39db6e9194ec74
Parth Suthar <104064849+ParthSuthar1012@users.noreply.github.com>
2024-05-16 18:14:40 +0530

    Update match-create.component.ts

--- src/app/match-create/match-create.component.ts
+++ src/app/match-create/match-create.component.ts
@@ -12,9 +12,9 @@ export class MatchCreateComponent implements OnInit {
   user: any;
   matchForm!: FormGroup;
   groups: any[] = [
-    { groupId: 1, name: 'Group A', listOfMembers: [{ name: 'test1', Id: 'gfgd12345' }] },
-    { groupId: 2, name: 'Group B', listOfMembers: [{ name: 'test1', Id: 'gfgd12345' }, { name: 'test2', Id: 'gfgd1236' }] },
-    { groupId: 3, name: 'Group C', listOfMembers: [{ name: 'test3', Id: 'gfgd12345' }] }
+    { groupId: 1, name: 'Group A', listOfMembers: [{ name: 'test1', Id: 'gfgd12345' }, { name: 'test2', Id: 'gfgd1236' },{ name: 'test3', Id: 'gfgd1236' },{ name: 'test4', Id: 'gfgd1236' },{ name: 'test5', Id: 'gfgd1236' }] },
+    { groupId: 2, name: 'Group B', listOfMembers: [{ name: 'test1', Id: 'gfgd12345' }, { name: 'test2', Id: 'gfgd1236' },{ name: 'test3', Id: 'gfgd1236' },{ name: 'test4', Id: 'gfgd1236' },{ name: 'test5', Id: 'gfgd1236' }] },
+    { groupId: 3, name: 'Group C', listOfMembers: [{ name: 'test1', Id: 'gfgd12345' }, { name: 'test2', Id: 'gfgd1236' },{ name: 'test3', Id: 'gfgd1236' },{ name: 'test4', Id: 'gfgd1236' },{ name: 'test5', Id: 'gfgd1236' }] }
   ];
 
   constructor(private _data: DataService, private afAuth: AngularFireAuth, private fb: FormBuilder) { }
@@ -44,6 +44,18 @@ export class MatchCreateComponent implements OnInit {
 
   onSubmit() {
     if (this.matchForm.valid) {
+      // Generate shuffled array of player numbers
+      const playerNumbers1 = this.shuffleArray([2, 4, 5, 1, 3]);
+      const playerNumbers2 = this.shuffleArray([1, 3, 2, 5, 4]);
+
+      // Assign player numbers to members of selected groups
+      this.matchForm.value.group.forEach((selectedGroup: any) => {
+        selectedGroup.listOfMembers.forEach((member: any, index: number) => {
+          member.playerNo1 = playerNumbers1[index];
+          member.playerNo2 = playerNumbers2[index]; 
+        });
+      });
+
       // Submit the form data
       console.log(this.matchForm.value);
       // Reset the form
@@ -51,6 +63,13 @@ export class MatchCreateComponent implements OnInit {
     }
   }
 
+  shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
   onGroupSelectionChange(event: any) {
     const selectedGroups = event.source.value;
     // Update form value with selected groups
